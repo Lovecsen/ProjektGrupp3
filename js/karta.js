7ea28b69af8877c9bff8bec0269afb508cd796db
Love <lh224nd@student.lnu.se>
2025-05-10 11:17:36 +0200

    Lagt in karta på list och produkt

--- js/karta.js
+++ js/karta.js
@@ -1,6 +1,6 @@
 
-let map;
-let marker;
+let map; //objekt för kartan
+let marker; //objekt för markör på kartan
 
 function init() {
     initMap("map");
@@ -9,9 +9,11 @@ function init() {
 window.addEventListener("load", init);
 
 function initMap(id) {
+
+    //nytt kartobjekt
     map = L.map(id).setView([57.353,15.601], 7);
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-    marker = L.marker([0, 0]).addTo(map);
+    marker = L.marker([0, 0]).addTo(map); //ny markör
 
 }
